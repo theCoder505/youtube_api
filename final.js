e34010b2048eb19807f2c274d1f73b6382b41a80
A.H.K. EMAD <68850204+theCoder505@users.noreply.github.com>
2021-08-26 02:36:19 +0600

    Add files via upload

--- final.js
+++ final.js
@@ -6,6 +6,7 @@ function keyWordsearch() {
 }
 
 function makeRequest() {
+    $(".loader").removeClass("d-none");
     var request = gapi.client.youtube.search.list({
         q: encodeURIComponent($("#query").val()).replace(/%20/g, "+"),
         part: 'snippet',
@@ -14,15 +15,29 @@ function makeRequest() {
         order: "viewCount"
     });
     request.execute(function(response) {
-        $('#results').empty()
+        $('#results').empty();
+        $('#results').append('<div class="loader d-none"></div>');
         var srchItems = response.result.items;
+        console.log(srchItems.length);
         $.each(srchItems, function(index, item) {
             vidTitle = item.snippet.title;
             videoSrcId = item.id.videoId;
             vidThumburl = item.snippet.thumbnails.default.url;
-            toShow = '<h2>' + vidTitle + '</h2> <iframe class="cstmYTVideo" src="//www.youtube.com/embed/' + videoSrcId + '" frameborder="0" allowfullscreen></iframe> <br><br>';
+            toShow = '<h3>' + vidTitle + '</h3> <br> <iframe class="cstmYTVideo" src="//www.youtube.com/embed/' + videoSrcId + '" frameborder="0" allowfullscreen></iframe> <br><br><hr><hr><br>';
 
             $('#results').append(toShow);
-        })
-    })
+        });
+        if (srchItems.length === 0) {
+            // alert("No Such Result Found");
+            $('#results').append("<div class='centerAlert'><h2> No Such Result Found </h2></div>");
+        }
+    });
 }
+
+
+$("#query").keypress(function(e) {
+    if (e.which == 13) {
+        // do work
+        $("#search-button").click();
+    }
+});
